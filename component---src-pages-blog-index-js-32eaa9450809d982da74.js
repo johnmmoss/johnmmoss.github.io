b1c3c22fa036627a19ceec39bada0a4cba944937
John Moss <john.moss@ranplanwireless.com>
2022-04-15 21:39:19 +0100

    Convert blog index page to use bootstrap cards

--- component---src-pages-blog-index-js-32eaa9450809d982da74.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[524],{1025:function(e,t,a){a.r(t);var n=a(7294),r=a(8500),l=a(1597);t.default=function(e){var t=e.data;return n.createElement(r.Z,{pageTitle:"Blog Page"},t.allMdx.nodes.map((function(e){return n.createElement("article",{key:e.id},n.createElement(l.rU,{to:"/blog/"+e.slug},e.frontmatter.title),n.createElement("p",null,"Posted: ",e.frontmatter.date))})))}}}]);
-//# sourceMappingURL=component---src-pages-blog-index-js-32eaa9450809d982da74.js.map
