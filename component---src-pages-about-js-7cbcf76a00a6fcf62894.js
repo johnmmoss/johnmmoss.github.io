f01994482738783b3bad5ded08710a6ed1121b27
John Moss <john.moss@ranplanwireless.com>
2022-04-26 21:00:52 +0100

    Add sticky footer

---
+++ component---src-pages-about-js-7cbcf76a00a6fcf62894.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[682],{4633:function(e,t,n){n.r(t);var s=n(7294),a=n(1327);t.default=function(){return s.createElement(a.Z,{pageTitle:"About Me"},s.createElement("p",null,"Hi, I'm John and this is my website"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-7cbcf76a00a6fcf62894.js.map
---
+++ component---src-pages-index-js-ca51b818377f4007dfea.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[678],{6558:function(e,t,a){a.r(t);var l=a(7294),s=a(1327);t.default=function(){return l.createElement(s.Z,{pageTitle:"Homepage"},l.createElement("p",null,"Hello this is my homepage. I still have abit more to do on it!"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-ca51b818377f4007dfea.js.map
